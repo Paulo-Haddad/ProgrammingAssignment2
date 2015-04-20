a3e6f8e3f54e1dc198a193549a6104ba962a1c5a
Paulo Haddad <pmiguelhm@gmail.com>
2015-04-20 18:49:44 -0300

    Finished assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+##############################################################################################################
+# Matrix inversion can be time-consuming, especially if it is going to be done inside a loop.                #
+# Instead of computing it repeatedly, one can cache the inverse of a matrix once and call it when necessary. #
+# The pair of functions presented below calculate and cache the inverse of a matrix.                         #
+# When appropriate, the inverse matrix is retrieved from the cache, and not recalculated.                    #
+##############################################################################################################
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+# makeCacheMatrix creates a list of functions that can cache a matrix inverse. #
+
+makeCacheMatrix <- function(original_matrix = matrix()) {
+        inverse_matrix <- NULL
+        # original_matrix <- input
+        # inverse_matrix <- NULL
+        set <- function(input) {
+                original_matrix <<- input
+                inverse_matrix <<- NULL
+        }
+        get <- function() original_matrix
+        # inverse_matrix <- calculated_i_mtx
+        set_i_mtx <- function(calculated_i_mtx) inverse_matrix <<- calculated_i_mtx
+        get_i_mtx <- function() inverse_matrix
+        list(set = set, get = get, set_i_mtx = set_i_mtx, get_i_mtx = get_i_mtx)
 
 }
 
 
-## Write a short comment describing this function
+# cacheSolve checks if the inverse matrix has already been calculated. If so, it returns the inverse value from the cache. #
+# If the inverse has not been calculated, then cacheSolve calculates it and store it in the cache (using makeCacheMatrix). #
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inverse_matrix <- x$get_i_mtx()
+        if(!is.null(inverse_matrix)) {
+                message("inverse matrix already calculated. getting cached data")
+                return(inverse_matrix)
+        }
+        data <- x$get()
+        inverse_matrix <- solve(data, ...)
+        x$set_i_mtx(inverse_matrix)
+        inverse_matrix
 }
